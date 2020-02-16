5c4267bfa17185f3d3152314f0b21b2c330b4abe
SatanicTriangle <satanictriangle@ubuntu.dev>
2020-02-16 14:31:01 +0100

    styled symbols in PlotData

--- Header/PlotDataSet.hpp
+++ Header/PlotDataSet.hpp
@@ -14,7 +14,7 @@ private:
 public:
     void setData(std::list<std::pair<double, double>> DataSet);
     void modifyDataSet(std::function<void(std::list<std::pair<double, double>>&)> lambda);
-    PlotDataSet(std::list<std::pair<double, double>> DataSet, char Symbol = 'o', Style Style = Style::dots);
+    PlotDataSet(std::list<std::pair<double, double>> DataSet, char Symbol = 'o', Color color = Color::non, Style Style = Style::dots);
     std::tuple<double,double,double,double> getRange() const;
     const std::list<std::pair<double,double>>& getData() const;
 };  
--- Source/PlotData.cpp
+++ Source/PlotData.cpp
@@ -1,12 +1,44 @@
 #include "../Header/PlotData.hpp"
 #include "../Header/Plot.hpp"
 
-PlotData::PlotData (char Symbol, Style Style) 
-: symbol(Symbol), style(Style), min_x(nan("")), max_x(nan("")), min_y(nan("")), max_y(nan(""))
-{}
+PlotData::PlotData (char Symbol, Style Style, Color Color) 
+: symbol(Symbol), style(Style), color(Color), min_x(nan("")), max_x(nan("")), min_y(nan("")), max_y(nan(""))
+{
+    setStyledSymbol();
+}
 
 PlotData::~PlotData(){
 }
 
-
-
+    std::string PlotData::mapColor(Color color){
+        switch(color){
+            case Color::RED:
+                return "\033[91m";
+            case Color::GREEN:
+                return "\033[92m";
+            case Color::BLUE:
+                return "\033[94m";
+            default: 
+                return "\033[39m";
+        }
+    }
+    char PlotData::getSymbol(){
+        return symbol;
+    }
+    void PlotData::setSymbol(char Symbol){
+        symbol = Symbol;
+        setStyledSymbol();
+    }
+    Color PlotData::getColor(){
+        return color;
+    }
+    void PlotData::setColor(Color Color){
+        color = Color;
+        setStyledSymbol();
+    }
+    const std::string& PlotData::getStyledSymbol(){
+        return styledSymbol;
+    }
+    void PlotData::setStyledSymbol(){
+        styledSymbol = mapColor(color) + symbol;
+    }
