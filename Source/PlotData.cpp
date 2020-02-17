44795d6321c642c49340f8a2cbf4b9f7ced7e2e8
SatanicTriangle <satanictriangle@ubuntu.dev>
2020-02-17 13:02:47 +0100

    More colors added

--- Source/PlotData.cpp
+++ Source/PlotData.cpp
@@ -18,6 +18,24 @@ PlotData::~PlotData(){
                 return "\033[92m";
             case Color::BLUE:
                 return "\033[94m";
+            case Color::GREY:
+                return "\033[90m";
+            case Color::YELLOW:
+                return "\033[93m";
+            case Color::PINK:
+                return "\033[95m";
+            case Color::DARK_GREY:
+                return "\033[30m";
+            case Color::DARK_RED:
+                return "\033[31m";
+            case Color::DARK_GREEN:
+                return "\033[32m";
+            case Color::ORANGE:
+                return "\033[33m";
+            case Color::DARK_BLUE:
+                return "\033[34m";
+            case Color::DARK_PINK:
+                return "\033[35m";
             default: 
                 return "\033[39m";
         }
