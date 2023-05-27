10975d2612726cccce35259d1df6a9f52ca3362e
Jacek Wierzowiecki <cubeMan217@gmail.com>
2023-05-27 20:43:41 +0200

    tests and micro hack for window size

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,12 +1,13 @@
 #include <ChartLib/Plot.hpp>
 #include <ChartLib/PlotDataSet.hpp>
+#include <ChartLib/IDecoration.hpp>
 
 #include <catch2/catch_test_macros.hpp>
 
 #include <cctype>
 
 // TODO: CTest?
-TEST_CASE("Interaction with PlotData" "[Plot]")
+TEST_CASE("Interaction with PlotData object" "[Plot]")
 {
     chart::Plot sut({10,10});
     sut.setVisibleRange({-2.0,2.0},{-1.5,1.5});
@@ -57,7 +58,43 @@ TEST_CASE("Interaction with PlotData" "[Plot]")
     }
 }
 
-TEST_CASE("Interaction with PlotData" "[Legend]")
+class TestingDecoration : public chart::IDecoration
 {
+    virtual std::tuple<int,int,int,int> drawFrame(
+        std::map<std::pair<int,int>, std::string>& ChartMap,
+        const std::pair<uint16_t,uint16_t>& WindowSize,
+        const std::tuple<double,double,double,double>& VisibleRange)
+    {
+        return {-1,WindowSize.first,0,WindowSize.second+1};
+    }
     
+    virtual bool isForced() const
+    {
+        return true;
+    };
+    virtual ~TestingDecoration() = default;
+};
+
+TEST_CASE( "Drawing has proper size", "[Drawing]")
+{
+    const int x=10 ,y=10;
+    chart::Plot sut({x,y});
+    chart::IDecoration* decoration = new TestingDecoration();
+
+    SECTION("Empty plot")
+    {
+        std::string out = sut.print();
+        // + y because of \ns 
+        REQUIRE(out.size() == x*y + y);
+    }
+    SECTION("Decoration expanding size")
+    {
+        sut.addDecoration(decoration);
+        std::string out = sut.print();
+        std::cout << "---\n" <<  out << "---\n";
+        REQUIRE(out.size() == (x+1)*(y+1) + y+1);
+    }
+
+    // TODO: get rid of row pointers on interface
+    delete decoration;
 }
